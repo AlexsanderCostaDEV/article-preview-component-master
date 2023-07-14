510657270fb78518b6584ffcef12c5b8e7abf554
AlexsanderCostaDEV <alexsander0souza@gmail.com>
2023-07-14 12:56:19 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// // sub-main é a classe da div invisivel
+// const div = document.getElementById("sub-main");
+
+// // getComputedStyle puxa dos styles da div
+// function showScreen() {
+//     const computedStyle = getComputedStyle(div);
+//     if (computedStyle.display === "none") {
+//         div.classList.add("tela");
+//     } else {
+//         div.classList.remove("tela");
+//     }
+// }
